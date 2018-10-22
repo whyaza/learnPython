dfd698ed8237b47b0162d4840184c5d7c22f7c6c
whyaza <645112728@qq.com>
2018-10-22 12:33:08 +0800

    update_cell

--- leetcode_python/cellmachine.py
+++ leetcode_python/cellmachine.py
@@ -6,6 +6,9 @@ import copy
 cells = []
 HEIGHT = os.get_terminal_size().lines
 WIDTH = os.get_terminal_size().columns
+models = 1 #默认模式1
+speed = 0.1 #默认速度0.1
+
 class OuterScreenException(Exception):
     pass
 
@@ -28,12 +31,28 @@ def init():
         elif c == curses.KEY_RIGHT:
             stdscr.move(y,x+1)
 
+def randomcells():
+    global cells
+    ro = random.randint(1,100)
+    #print(HEIGHT)
+    #print(WIDTH)
+    for i in range(20,HEIGHT - 20):
+        for j in range(20,WIDTH - 20):
+            if ro < 50 :
+                cells.append([i,j,True])
+                stdscr.addstr(i,j,'@')
+            ro = random.randint(1,100)
+    
+    stdscr.refresh()
+
 def draw():
     stdscr.erase()
     stdscr.refresh()
     try :
         for cell in cells:
             stdscr.addstr(cell[0],cell[1],'@')
+        back_info = '      按b，回到主菜单'
+        stdscr.addstr(HEIGHT-5,WIDTH-5,back_info,curses.A_BOLD)
         stdscr.refresh()
     except :
         raise OuterScreenException
@@ -48,7 +67,9 @@ def play():
     poss = ((-1,-1),(0,-1),(1,-1),(-1,0),(1,0),(-1,1),(0,1),(1,1))
     global cells
     while len(cells):
-        
+        c = stdscr.getch()
+        if c == ord('b'):
+            return 
         for ls in range(len(cells)):
             for pos in poss:
                 tempx = cells[ls][1] + pos[0]
@@ -83,39 +104,47 @@ def play():
             draw()
         except OuterScreenException as e:
             return
-        time.sleep(1)
-        
+        time.sleep(speed)
+
 def main(stdscr):
-    global cells
+    global cells,models,speed
     while True:
+        curses.noecho()
+        curses.cbreak()
+        stdscr.keypad(1)
+        stdscr.box()
+        stdscr.addstr(10,10,info,curses.A_BOLD)
+
+        stdscr.nodelay(0)
+        c = stdscr.getch()
+        if c == ord('1'):
+            models = 1
+            speed = 0.1
+        if c == ord('2'):
+            models = 2
+            speed = 0.5
+
         cells = []
         stdscr.erase()
         stdscr.move(HEIGHT//2,WIDTH//2)
-        init()
+        #设定两种模式，自动模式和自定义模式：
+        if models == 1:   #开始自动模式
+            randomcells()    
+        if models == 2:
+            init()
+        stdscr.nodelay(1)
         play()
+        stdscr.erase()
+
 
 info = '''
-            细胞自动机游戏说明:
-            1.按b/B键开始游戏,
-            2.键盘上下左右控制光标移动,空格设置初始细胞,o/O键开始启动细胞自动机
-            3.细胞自动机如果细胞移动出屏幕 or len(细胞) == 0时刻,会自动关闭
-            4.祝您玩的愉快!~
+                                                                            细胞自动机游戏说明:
+                                                            1.按1/2键选择模式,１:自动模式,2:自定义模式
+                                                            2.自定义模式下:键盘上下左右控制光标移动,空格设置初始细胞,o/O键开始启动细胞自动机
+                                                            3.细胞自动机如果细胞移动出屏幕 or len(细胞) == 0时刻,会自动关闭
+                                                                            祝您玩的愉快!~
 '''
 
 stdscr = curses.initscr()
-curses.noecho()
-curses.cbreak()
-stdscr.keypad(1)
-stdscr.box()
-stdscr.addstr(10,10,info,curses.A_BOLD)
-while True:
-    c = stdscr.getch()
-    if c == ord('B') or c == ord('b'):
-        break
-stdscr.erase()
 stdscr.move(HEIGHT//2,WIDTH//2)
 curses.wrapper(main)
-
-
-
-
